70a4cd447e85dfd472692d08b3aefcb898b88b66
Hayden Feddock <hayden4feddock@gmail.com>
2024-05-21 14:57:41 -0400

    package formatting

---
+++ train_system/common/track.py
@@ -0,0 +1,6 @@
+# train_system/common/track.py
+
+class Track:
+    def __init__(self, track_id):
+        self.track_id = track_id
+
---
+++ train_system/common/train.py
@@ -0,0 +1,5 @@
+# train_system/common/train.py
+
+class Train:
+    def __init__(self, train_id):
+        self.train_id = train_id
