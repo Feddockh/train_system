67c9cc5a37a443acc02ac03075b65e9b0aa80492
Arissa <ajb321@pitt.edu>
2024-06-18 14:12:50 -0400

    ui-test bench window with position inputs sending, mbo mbo mode view button. main- makes 3 schedule text files with headers, km/hr to m/s conversion, and some more general outlining

--- train_system/mbo_manager/mbo_manager.py
+++ train_system/mbo_manager/mbo_manager.py
@@ -1,1 +1,125 @@
-#test github connection
+
+class MBOController:
+    def __init__(self):
+        """
+        Initialize the MBO Controller
+        """
+        self.train_commanded_speeds = {}
+        self.train_authorities = {}
+        self.block_maintanece = {}
+        
+        self.dispatch_mode = {} #manual and mbo 
+        self.mbo_mode = {}  #fixed block or mbo 
+        self.enable_s_and_a = 0
+        
+        self.lines = {'Blue'}
+        self.blue_speed_limit = 50 #km/hr, convert to m/s
+        
+        self.trains_positions = {"Train1" : 0}
+   
+        self.stations = ["Yard", "Station B", "Station C"]
+        self.drivers = ["Driver 1", "Driver 2", "Driver 3"]
+        self.crew = ["Crew 1", "Crew 2", "Crew 3", "Crew 4", "Crew 5", "Crew 6"]
+        
+       
+    def kmhr_to_ms(self, blue_speed_limit):
+        return(self.blue_speed_limit * (1000/3600))
+        
+    def distance_between(self): #?? idk, or time to travel based of distance between?
+        """
+        finding the distance in bettween 
+            stations?
+            
+        """ 
+        #yard to b = 500m 
+        #b to c = 
+    
+    def create_schedules(self, selected_day, selected_start_time):
+        """
+        Create schedule options 
+        """
+        #where does hayden want me to save these files? send to folder in ctc folder? label schedules? 
+        #schedule as txt? cvs? excel? 
+        
+        #printing to command for demo 
+        print('making schedule for: ', selected_day)
+        print('starting schedule at ', selected_start_time)
+        
+        #creating all 3 schedule options 
+        low_throughput_filename = selected_day + '_low.txt'
+        med_throughput_filename = selected_day + '_med.txt'
+        high_throughput_filename = selected_day + '_high.txt'
+        
+        #creating file names for all three schedule types  
+        low_file = open(low_throughput_filename, 'w')
+        med_file = open(med_throughput_filename, 'w')
+        high_file = open(high_throughput_filename, 'w')
+        
+        #headers for each file 
+        low_file.write('Train      Line    Station    Arrival Time    Departure Time    Driver    Crew 1    Crew 2    Crew 3\n')
+        med_file.write('Train      Line    Station    Arrival Time    Departure Time    Driver    Crew 1    Crew 2    Crew 3\n')
+        high_file.write('Train      Line    Station    Arrival Time    Departure Time    Driver    Crew 1    Crew 2    Crew 3\n')
+        
+        #all crew need a break after 4 hours 
+        #.5 hour break 
+        #8.5 hour shifts total 
+        
+        #total schedule needs to go from start time + 24 hours 
+        
+        #initial speed and authority on schedule for CTC? I think not necessary since CTC has ability to calculate this on their own? 
+        
+        for i in range(1,5):
+            low_file.write('Train ' + repr(i) + '   ' + repr(self.lines) + '   Station B   ' + repr(selected_start_time) + '    ' + repr(selected_start_time) +'\n')
+        
+        
+        #closing files when finished 
+        low_file.close()
+        med_file.close()
+        high_file.close()
+        
+     
+    def enable_mbo_mode(self, dispatch_mode, enable_s_and_a):
+        """
+        If dispatcher selects MBO mode, enable sending speed and authority 
+        """  
+        #print in command prompt for test bench purposes? 
+        
+        if self.dispatch_mode == 'MBO': 
+            self.enable_s_and_a = 1;  
+        
+        else:
+            self.enable_s_and_a = 0;
+        
+    def commanded_speed(self, enable_s_and_a ):
+        """
+        Calculate trains commanded speed 
+        """
+        if(enable_s_and_a):
+        #do i need to adjust speed when train needs to stop? 
+        
+            return(self.blue_speed_limit)
+        
+    def authorirty(self):
+        """
+        Calculate trains authority such that more than one train can be in a block 
+        """
+        print(self.trains_positions)
+        #leaving yard authorirty is 250 m 
+        #station b to station c authority?
+        #station c to yard is 250 m 
+        
+        #if entering a block and the next block is under maintance, authority = length of block 
+        
+        #if train is x meters away from the train infront of it, set authority to emergency breaking distance?
+            #emergency break = 2.73m/s2
+            #how far will train travel if break is applied while traveling 50km/hr 
+            
+    
+        
+
+if __name__ == "__main__":
+    MBO = MBOController()
+    speed_limit = MBO.blue_speed_limit
+    m = MBO.kmhr_to_ms(speed_limit)
+    print(m)
+    
--- train_system/mbo_manager/mbo_ui.py
+++ train_system/mbo_manager/mbo_ui.py
@@ -1,3 +1,4 @@
+from PyQt6.QtCore import Qt
 from PyQt6.QtGui import * 
 from PyQt6.QtWidgets import *
 from PyQt6.QtCore import *
@@ -6,34 +7,188 @@ from datetime import datetime
 import sys
 
 
+from PyQt6.QtWidgets import QWidget
+
+from mbo_manager import MBOController
+
 class MBOWindow(QMainWindow):
     def __init__(self):
         super(MBOWindow, self).__init__()
         
+        #name window
         self.setWindowTitle("MBO Controller")
-
-        layout = QVBoxLayout()
         
+        self.MBO_mode_window = None
+        #button to navigate to MBO mode view, see trains postitions, commanded speed and authority in real time? 
+        self.MBO_mode_view = QPushButton('MBO Mode View', self)
+        self.MBO_mode_view.clicked.connect(self.open_MBO_mode_view)
+        
+        self.test_bench_window = None
+        #button to navigate to test bench view 
+        self.test_bench_view = QPushButton('Test Bench')
+        self.test_bench_view.clicked.connect(self.open_test_bench_view)
+        
+        #display txt file of schedule? 
+        
+        #test bench - button and page 
+        
+        
+        #lable to prompt/dircetion for user to enter information
         self.enter_label = QLabel('Please enter the date and time for the new schedules.')
         
+        #creating calendar pop up and time
         self.schedule_date_time = QDateTimeEdit(datetime.now(),self)
         self.schedule_date_time.setCalendarPopup(True)
         
+        #Creat New Schedules Button, when clicked calls create sched function 
         self.create_schedules = QPushButton('Create New Schedules', self)
         self.create_schedules.clicked.connect(self.save_schedule_date_time)
         
+        #init layout for creating schedule
+        self.schedule_layout = QVBoxLayout()
+        #setting layout of the page
+        self.schedule_layout.addWidget(self.MBO_mode_view)
+        self.schedule_layout.addWidget(self.enter_label)
+        self.schedule_layout.addWidget(self.schedule_date_time)
+        self.schedule_layout.addWidget(self.create_schedules)
+        self.schedule_layout.addWidget(self.test_bench_view)
         
-        layout.addWidget(self.enter_label)
-        layout.addWidget(self.schedule_date_time)
-        layout.addWidget(self.create_schedules)
-    
         widget = QWidget()
-        widget.setLayout(layout)   
-        self.setCentralWidget(widget)     
+        widget.setLayout(self.schedule_layout)
+        self.setCentralWidget(widget)
         
+        
+    #calling function to create schedules 
     def save_schedule_date_time(self):
-        selected_day_time = self.schedule_date_time.dateTime().toString('MM-dd-yyyy HH:mm')
-        print('current date and time is', selected_day_time)       
+        """
+        Format and send user entered date and time to mbo_manager file to create schedule 
+        """
+        self.selected_day = self.schedule_date_time.dateTime().toString('MM-dd-yyyy')
+        self.selected_start_time = self.schedule_date_time.dateTime().toString('HH:mm')
+        x = MBOController()   
+        x.create_schedules(self.selected_day, self.selected_start_time)
+    
+    #opening MBO mode view window
+    def open_MBO_mode_view(self):
+        if self.MBO_mode_window is None:
+            self.MBO_mode_window = MBOModeView()
+            self.MBO_mode_window.show()
+        else:
+            self.MBO_mode_window.close()
+            self.MBO_mode_window = None
+    
+    #opening test bench view for MBO mode view window 
+    def open_test_bench_view(self):
+        if self.test_bench_window is None:
+            self.test_bench_window = TestBench()
+            self.test_bench_window.show()
+        else:
+            self.test_bench_window.close()
+            self.test_bench_window = None
+        
+    
+
+class MBOModeView(QWidget):
+    def __init__(self):
+        super(MBOModeView, self).__init__()
+        
+        self.setWindowTitle("MBO Mode View")
+        
+        self.MBO_mode_view = QPushButton('MBO Mode View', self)
+        self.MBO_mode_view.clicked.connect(self.close_MBO_mode_view)
+    
+    def close_MBO_mode_view(self):
+        self.window = MBOModeView()
+        #way to close window, back button!
+        
+
+class TestBench(QMainWindow):
+    def __init__(self):
+        super(TestBench,self).__init__()
+        
+        #label for page window 
+        self.setWindowTitle("MBO Test Bench")
+        
+        #labels for table header
+        self.trains_header = QLabel('Trains')
+        self.position_header = QLabel('Position')
+        self.authority_header = QLabel('Authority')
+        self.speed_header = QLabel('Commanded Speed')
+        
+        #aligning header
+        self.header_layout = QHBoxLayout()
+        self.header_layout.addWidget(self.trains_header)
+        self.header_layout.addWidget(self.position_header)
+        self.header_layout.addWidget(self.authority_header)
+        self.header_layout.addWidget(self.speed_header)
+        
+        
+        #labels for train positions 
+        self.train1_position_label = QLabel("Train 1")
+        self.train2_position_label = QLabel("Train 2")
+        self.train3_posotion_label = QLabel("Train 3")
+        
+        #text boxes to edit train position
+        self.train1_position = QTextEdit()
+        self.train2_position = QTextEdit()
+        self.train3_position = QTextEdit()
+    
+        
+          
+        #button to run test bench
+        self.test = QPushButton("Run Test")
+        self.test.clicked.connect(self.run_test_bench)
+        
+        #setting up layout to align position labels 
+        self.layout_position_labels = QVBoxLayout()
+        self.layout_position_labels.addWidget(self.train1_position_label)
+        self.layout_position_labels.addWidget(self.train2_position_label)
+        self.layout_position_labels.addWidget(self.train3_posotion_label)
+        
+        #setting up layout to align text edit boxes 
+        self.layout_position_text = QVBoxLayout()
+        self.layout_position_text.addWidget(self.train1_position)
+        self.layout_position_text.addWidget(self.train2_position)
+        self.layout_position_text.addWidget(self.train3_position)
+        
+
+        self.column_layout = QHBoxLayout()
+        self.column_layout.addLayout(self.layout_position_labels)
+        self.column_layout.addLayout(self.layout_position_text)
+        
+        self.table_layout = QVBoxLayout()
+        self.table_layout.addLayout(self.header_layout)
+        self.table_layout.addLayout(self.column_layout)
+        
+        self.test_layout = QVBoxLayout()
+        self.test_layout.addWidget(self.test)
+        
+        #aligning labels and text edit boxes 
+        self.window_layout = QVBoxLayout()
+        self.window_layout.addLayout(self.table_layout)
+        self.window_layout.addLayout(self.test_layout)
+        
+        main_widget = QWidget()
+        main_widget.setLayout(self.window_layout)
+        self.setCentralWidget(main_widget)
+        
+    
+        
+        #enter in dispatch mode? 
+        #show authority and commanded speed being enabled and disabled?
+    
+    def run_test_bench(self):
+        x = MBOController() 
+        
+        #need a check to make sure these text boxes are not empty! will crash! 
+        
+        #send out positions 
+        x.trains_positions = {"Train1": int(self.train1_position.toPlainText()), "Train2": int(self.train2_position.toPlainText()), "Train3": int(self.train3_position.toPlainText())}
+        x.authorirty()
+        
+        
+        
+        
        
 
 app = QApplication(sys.argv)
