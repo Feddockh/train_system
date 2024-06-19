0c7b9fceaf723f0969627d11595d9707e9e33e68
Arissa <ajb321@pitt.edu>
2024-06-19 13:33:07 -0400

    authority is working! test bench mostly complete, fixed up UI design

--- train_system/mbo_manager/mbo_manager.py
+++ train_system/mbo_manager/mbo_manager.py
@@ -1,3 +1,4 @@
+from csv import writer
 
 class MBOController:
     def __init__(self):
@@ -10,9 +11,9 @@ class MBOController:
         self.enable_s_and_a = 0
         
         self.lines = {'Blue'}
-        self.blue_speed_limit = 50 #km/hr, convert to m/s
+        self.blue_speed_limit = 50.0 #km/hr, convert to m/s
         
-        self.trains_positions = {"Train1" : 0}
+        self.test_trains_positions = {"Train1" : 0}
    
         self.stations = ["Yard", "Station B", "Station C"]
         self.drivers = ["Driver 1", "Driver 2", "Driver 3"]
@@ -25,7 +26,8 @@ class MBOController:
         Args:
             km_hr (float?): km/hr that needs to be converted to m/s, mostly for setting commanded speed based of speed limit 
         """
-        return(km_hr * (1000/3600))
+        ms = km_hr * (1000.00/3600.00)
+        return(ms)
     
     def ms_to_mph(self, ms):
         """
@@ -39,11 +41,11 @@ class MBOController:
      distance the train will travel after emergency break is pulled
         (+ some wiggle room? )
      """ 
-     emergency_brake_acceleration = 2.73 #m/s^2
+     emergency_brake_acceleration = 2.73
+     v = 0.0
      v = self.commanded_speed(self.enable_s_and_a)
      breaking_distance = -1* (1/2)*(v)*(-1 * emergency_brake_acceleration)
     
-     print(breaking_distance)
      return (breaking_distance)
     
     def enable_mbo_mode(self, dispatch_mode, enable_s_and_a):
@@ -53,10 +55,10 @@ class MBOController:
         #print in command prompt for test bench purposes? 
         
         if self.dispatch_mode == 'MBO': 
-            self.enable_s_and_a = 1;  
+            self.enable_s_and_a = 1  
         
         else:
-            self.enable_s_and_a = 0;
+            self.enable_s_and_a = 0
         
     def commanded_speed(self, enable_s_and_a ):
         """
@@ -69,15 +71,52 @@ class MBOController:
         if(enable_s_and_a):
         #do i need to adjust speed when train needs to stop? 
             self.speed = self.kmhr_to_ms(self.blue_speed_limit)
-            return(self.speed)
+        else:
+            self.speed = 0
+            
+        return(self.speed)
     
     
-    def authorirty(self):
+    def authority(self, trains_positions, block_maint):
         """
         Calculate trains authority such that more than one train can be in a block 
         """
-        print(self.trains_positions)
+        trains = list(trains_positions.keys())
+        number_of_trains = len(trains)
         
+        blocks = list(block_maint.values())
+        number_of_block_maint = len(block_maint)
+        
+        authorities = {}
+        
+        for i in range(number_of_trains):
+            train_1 = trains[i]
+            position_1 = trains_positions[train_1]
+            
+            authorities[train_1] = abs(position_1-500)
+            
+            
+            for j in range(i+1, number_of_trains):
+                train_2 = trains[j]
+                position_2 = trains_positions[train_2]
+                distance_from_train = abs(position_1 - position_2)
+                
+                if(distance_from_train <= float(self.emergency_breaking_distance())):
+                    authorities[train_1] = round(self.emergency_breaking_distance())
+                
+                else: 
+                    if(number_of_block_maint > 0 ):
+                        for x in range(number_of_block_maint):
+                            block_position = blocks[x]
+                            to_block = abs(position_1 - block_position)
+                            if (to_block <= 50):
+                                authorities[train_1] = round(self.emergency_breaking_distance())
+                    
+                
+        
+        return (authorities)
+                    
+       
         #for each train 
             #if train is within emerg braking distance of another train 
                 #then authority = emerg breaking distance + c
@@ -91,6 +130,9 @@ class MBOController:
     def create_schedules(self, selected_day, selected_start_time):
         """
         Create schedule options 
+        
+        return bool to ensure schedules were made - make pop up window in UI to confirm they were made successfully 
+                                                        give file path for them
         """
         #where does hayden want me to save these files? send to folder in ctc folder? label schedules? 
         #schedule as txt? cvs? excel? 
@@ -100,25 +142,40 @@ class MBOController:
         print('starting schedule at ', selected_start_time)
         
         #creating all 3 schedule options 
-        low_throughput_filename = selected_day + '_low.txt'
-        med_throughput_filename = selected_day + '_med.txt'
-        high_throughput_filename = selected_day + '_high.txt'
+        low_throughput_filename = selected_day + '_low.csv'
+        med_throughput_filename = selected_day + '_med.csv'
+        high_throughput_filename = selected_day + '_high.csv'
         
         #creating file names for all three schedule types  
         low_file = open(low_throughput_filename, 'w')
         med_file = open(med_throughput_filename, 'w')
         high_file = open(high_throughput_filename, 'w')
         
+        low_writer = writer(low_file)
+        med_writer = writer(med_file)
+        high_writer = writer(high_file)
+        
+        
         #headers for each file 
-        low_file.write('Train      Line    Station    Arrival Time    Departure Time    Driver    Crew 1    Crew 2    Crew 3\n')
-        med_file.write('Train      Line    Station    Arrival Time    Departure Time    Driver    Crew 1    Crew 2    Crew 3\n')
-        high_file.write('Train      Line    Station    Arrival Time    Departure Time    Driver    Crew 1    Crew 2    Crew 3\n')
+        header = ['Train', 'Line', 'Station', 'Arrival Time', 'Driver', 'Crew 1', 'Crew 2']
+        low_writer.writerow(header)
+        med_writer.writerow(header)
+        high_writer.writerow(header)
+        
+        
         
         #all crew need a break after 4 hours 
         #.5 hour break 
         #8.5 hour shifts total 
-        
         #total schedule needs to go from start time + 24 hours 
+        
+        #low throughput schedule = 1 train 
+        
+        #med throughput schedule = 2 trains 
+        
+        #high throughput shcedule = 3 trains 
+        
+        
         
         #initial speed and authority on schedule for CTC? I think not necessary since CTC has ability to calculate this on their own? 
         
@@ -136,7 +193,33 @@ class MBOController:
 
 if __name__ == "__main__":
     MBO = MBOController()
-    speed_limit = MBO.blue_speed_limit
-    m = MBO.kmhr_to_ms(speed_limit)
-    print(m)
+    MBO.enable_s_and_a = 1
+    testing_positions_1 = {'Train1': 100, 'Train2': 300, 'Train3': 310}
+    testing_positions_2 = {'Train1': 100, 'Train2': 335, 'Train3': 350, 'Train4': 420}
+    block_maint_1 = {}
+    block_maint_2 = {'4': 150,'9': 400 }
+    
+    print('\n\nSpeed Limit: ', MBO.blue_speed_limit,'km/hr')
+    print('Emergency Breaking Distance At Speed Limit: ', MBO.emergency_breaking_distance(), 'm/s \n')
+    
+    print('----TEST 1: MBO MODE, NO MAINT.----')
+    print('Train Positions: ', testing_positions_1, '[m]')
+    print('No Blocks Under Maint.')
+    test_1_author = MBO.authority(testing_positions_1, block_maint_1)
+    print('Authorities: ', test_1_author)
+    print(' -Train1 authority = distance to station')
+    print(' -Train2 authority = emergency braking distance, train is an unsafe distance away from the train infront of it')
+    print(' -Train3 authority = distance to station\n')
+    
+    print('----TEST 2: MBO MODE, BLOCK MAINT.----')  
+    print('Train Positions: ',testing_positions_2, '[m]')
+    print('Block 4 Under Maint, Position 150 m')
+    test_2_author = MBO.authority(testing_positions_2, block_maint_2)
+    print('Authorities: ', test_2_author)
+    print(' -Train1 authority = emergency braking distance, must stop before block under maint.')
+    print(' -Train2 authority = emergency braking distance, train is an unsafe distance away from the train infront of it')
+    print(' -Train3 authority = emergency braking distance, must stop before block under maint.')
+    print(' -Train4 authority = distance to station\n')
+    
+    
     
--- train_system/mbo_manager/mbo_ui.py
+++ train_system/mbo_manager/mbo_ui.py
@@ -22,11 +22,15 @@ class MBOWindow(QMainWindow):
         self.MBO_mode_window = None
         #button to navigate to MBO mode view, see trains postitions, commanded speed and authority in real time? 
         self.MBO_mode_view = QPushButton('MBO Mode View', self)
+        self.MBO_mode_view.setFixedSize(150,50)
+        self.MBO_mode_view.setFont(QFont('Times', 12))
         self.MBO_mode_view.clicked.connect(self.open_MBO_mode_view)
         
         self.test_bench_window = None
         #button to navigate to test bench view 
         self.test_bench_view = QPushButton('Test Bench')
+        self.test_bench_view.setFont(QFont('Times', 12))
+        self.test_bench_view.setFixedSize(100,50)
         self.test_bench_view.clicked.connect(self.open_test_bench_view)
         
         #display txt file of schedule? 
@@ -36,13 +40,19 @@ class MBOWindow(QMainWindow):
         
         #lable to prompt/dircetion for user to enter information
         self.enter_label = QLabel('Please enter the date and time for the new schedules.')
+        self.enter_label.setFont(QFont('Times', 18))
+        self.enter_label.setFixedHeight(50)
         
         #creating calendar pop up and time
         self.schedule_date_time = QDateTimeEdit(datetime.now(),self)
+        self.schedule_date_time.setFixedSize(300,50)
+        self.schedule_date_time.setFont(QFont('Times', 12))
         self.schedule_date_time.setCalendarPopup(True)
         
         #Creat New Schedules Button, when clicked calls create sched function 
         self.create_schedules = QPushButton('Create New Schedules', self)
+        self.create_schedules.setFixedSize(300,100)
+        self.create_schedules.setFont(QFont('Times', 14))
         self.create_schedules.clicked.connect(self.save_schedule_date_time)
         
         #init layout for creating schedule
@@ -113,14 +123,24 @@ class TestBench(QMainWindow):
         self.setFixedSize(1222, 702)
         
         #labels for table header
-        self.trains_header = QLabel('Trains')
+        self.trains_header = QLabel('Train')
+        self.trains_header.setFixedSize(200,100)
+        self.line_header = QLabel('Line')
+        self.line_header.setFixedSize(200,100)
+        self.station_header = QLabel('Station')
+        self.station_header.setFixedSize(200,100)
         self.position_header = QLabel('Position')
+        self.position_header.setFixedSize(200,100)
         self.authority_header = QLabel('Authority')
+        self.authority_header.setFixedSize(200,100)
         self.speed_header = QLabel('Commanded Speed')
+        self.speed_header.setFixedSize(200,100)
         
         #aligning header
         self.header_layout = QHBoxLayout()
         self.header_layout.addWidget(self.trains_header)
+        self.header_layout.addWidget(self.line_header)
+        self.header_layout.addWidget(self.station_header)
         self.header_layout.addWidget(self.position_header)
         self.header_layout.addWidget(self.authority_header)
         self.header_layout.addWidget(self.speed_header)
@@ -128,29 +148,68 @@ class TestBench(QMainWindow):
         
         #labels for train positions 
         self.train1_position_label = QLabel("Train 1")
+        self.train1_position_label.setFixedSize(200,100)
         self.train2_position_label = QLabel("Train 2")
-        self.train3_posotion_label = QLabel("Train 3")
+        self.train2_position_label.setFixedSize(200,100)
+        self.train3_position_label = QLabel("Train 3")
+        self.train3_position_label.setFixedSize(200,100)
         
         #text boxes to edit train position
         self.train1_position = QTextEdit()
+        self.train1_position.setFixedSize(200,100)
         self.train2_position = QTextEdit()
+        self.train2_position.setFixedSize(200,100)
         self.train3_position = QTextEdit()
-    
+        self.train3_position.setFixedSize(200,100)
         
-        self.train1_commanded_speed = QLabel('-')
-        self.train2_commanded_speed = QLabel('-')
-        self.train3_commanded_speed = QLabel('-')
+        #line (all blue lol)
+        self.train1_line = QLabel("Blue")
+        self.train1_line.setFixedSize(200,100)
+        self.train2_line = QLabel("Blue")
+        self.train2_line.setFixedSize(200,100)
+        self.train3_line = QLabel("Blue")
+        self.train3_line.setFixedSize(200,100)
+        
+        
+        #stations (for blue line)
+        self.train1_station = QComboBox()
+        self.train1_station.addItems(['Station B', 'Station C', 'Yard'])
+        self.train1_station.setFixedSize(200,100)
+        self.train2_station = QComboBox()
+        self.train2_station.addItems(['Station B', 'Station C', 'Yard'])
+        self.train2_station.setFixedSize(200,100)
+        self.train3_station = QComboBox()
+        self.train3_station.addItems(['Station B', 'Station C', 'Yard'])
+        self.train3_station.setFixedSize(200,100)
+        
+    
+        #place holders for commanded speed 
+        self.train1_commanded_speed = QLabel('---')
+        self.train1_commanded_speed.setFixedSize(200,100)
+        self.train2_commanded_speed = QLabel('---')
+        self.train2_commanded_speed.setFixedSize(200,100)
+        self.train3_commanded_speed = QLabel('---')
+        self.train3_commanded_speed.setFixedSize(200,100)
+        
+        #place holders for authority 
+        self.train1_authority = QLabel('---')
+        self.train1_authority.setFixedSize(200,100)
+        self.train2_authority = QLabel('---')
+        self.train2_authority.setFixedSize(200,100)
+        self.train3_authority = QLabel('---')
+        self.train3_authority.setFixedSize(200,100)
         
           
         #button to run test bench
         self.test = QPushButton("Run Test")
+        self.test.setFixedSize(200,100)
         self.test.clicked.connect(self.run_test_bench)
         
         #setting up layout to align position labels 
         self.layout_position_labels = QVBoxLayout()
         self.layout_position_labels.addWidget(self.train1_position_label)
         self.layout_position_labels.addWidget(self.train2_position_label)
-        self.layout_position_labels.addWidget(self.train3_posotion_label)
+        self.layout_position_labels.addWidget(self.train3_position_label)
         
         #setting up layout to align text edit boxes 
         self.layout_position_text = QVBoxLayout()
@@ -158,16 +217,37 @@ class TestBench(QMainWindow):
         self.layout_position_text.addWidget(self.train2_position)
         self.layout_position_text.addWidget(self.train3_position)
         
+        #vert aligning of line
+        self.layout_line = QVBoxLayout()
+        self.layout_line.addWidget(self.train1_line)
+        self.layout_line.addWidget(self.train2_line)
+        self.layout_line.addWidget(self.train3_line)
+        
+        #vert aligining of stations 
+        self.layout_station = QVBoxLayout()
+        self.layout_station.addWidget(self.train1_station)
+        self.layout_station.addWidget(self.train2_station)
+        self.layout_station.addWidget(self.train3_station)
+        
         #vert aligning layout of commanded speeds 
         self.layout_speed = QVBoxLayout()
         self.layout_speed.addWidget(self.train1_commanded_speed)
         self.layout_speed.addWidget(self.train2_commanded_speed)
         self.layout_speed.addWidget(self.train3_commanded_speed)
         
+        #vert aligning layout of author
+        self.layout_authority = QVBoxLayout()
+        self.layout_authority.addWidget(self.train1_authority)
+        self.layout_authority.addWidget(self.train2_authority)
+        self.layout_authority.addWidget(self.train3_authority)
+        
         #horz aligning columns in table 
         self.column_layout = QHBoxLayout()
         self.column_layout.addLayout(self.layout_position_labels)
+        self.column_layout.addLayout(self.layout_line)
+        self.column_layout.addLayout(self.layout_station)
         self.column_layout.addLayout(self.layout_position_text)
+        self.column_layout.addLayout(self.layout_authority)
         self.column_layout.addLayout(self.layout_speed)
         
         self.table_layout = QVBoxLayout()
@@ -199,7 +279,7 @@ class TestBench(QMainWindow):
         #need a check to make sure these text boxes are not empty! will crash! 
         
         #send out positions 
-        x.trains_positions = {"Train1": int(self.train1_position.toPlainText()), "Train2": int(self.train2_position.toPlainText()), "Train3": int(self.train3_position.toPlainText())}
+        test_trains_positions = {"Train1": int(self.train1_position.toPlainText()), "Train2": int(self.train2_position.toPlainText()), "Train3": int(self.train3_position.toPlainText())}
         
         c =x.ms_to_mph( x.commanded_speed(x.enable_s_and_a) )
         self.train1_commanded_speed.setText(str(c))
@@ -207,10 +287,12 @@ class TestBench(QMainWindow):
         self.train3_commanded_speed.setText(str(c))
         
         x.emergency_breaking_distance()
+        block = {}
+        authorities = x.authority(test_trains_positions, block)
         
-        pass # x.authorirty()
-        
-        
+        self.train1_authority.setText(str(authorities['Train1']))
+        self.train2_authority.setText(str(authorities['Train2']))
+        self.train3_authority.setText(str(authorities['Train3']))
         
         
        
