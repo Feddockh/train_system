4ef81e1ad9acb686b2d949d0f366d9447efd45f8
Matt Discepola <mdiscep@icloud.com>
2024-07-18 15:29:41 -0400

    builder ui and track upload working

--- train_system/track_model/builder_view.py
+++ train_system/track_model/builder_view.py
@@ -1,23 +1,51 @@
-from PyQt6.QtWidgets import QWidget, QPushButton, QHBoxLayout, QVBoxLayout
+import os
+import sys
+from PyQt6.QtWidgets import QWidget, QFileDialog, QPushButton, QHBoxLayout, QVBoxLayout, QApplication
+from PyQt6.QtCore import QObject, pyqtSignal, Qt
 
 from train_system.common.line import Line
 from train_system.track_model.live_map import LiveMap
 
 class BuilderUI(QWidget):
 
-    def __init__(self, line: Line):
+    def __init__(self):
 
         super().__init__()
 
         self.big_layout = QHBoxLayout()
-        self.live_map = LiveMap(line)
+        self.live_map = LiveMap()
         self.upload_button = QPushButton('Upload Track')
         self.murphy_button = QPushButton('Murphy View')
+
+        self.upload_button.setFixedSize(170, 45)
+        self.murphy_button.setFixedSize(100, 30)
 
         self.button_layout = QVBoxLayout()
         self.button_layout.addWidget(self.upload_button)
         self.button_layout.addWidget(self.murphy_button)
+        self.button_layout.setAlignment(self.upload_button, Qt.AlignmentFlag.AlignCenter)
+        self.button_layout.setAlignment(self.murphy_button, Qt.AlignmentFlag.AlignCenter)
 
         self.big_layout.addLayout(self.button_layout)
         self.big_layout.addWidget(self.live_map)
         self.setLayout(self.big_layout)
+
+        self.upload_button.clicked.connect(self.open_file)
+
+    def open_file(self):
+        filename = QFileDialog.getOpenFileName(self, 'Select Track Model', './system_data/lines')
+        self.line = Line('Green')
+        self.line.load_track_blocks(filename[0])
+
+        self.live_map.set_line(self.line)
+
+if __name__ == "__main__":
+
+    #line = Line('Green')
+    #file_path = os.path.abspath(os.path.join("system_data/lines", "green_line.xlsx"))
+    #line.load_track_blocks(file_path)
+    
+    app = QApplication(sys.argv)
+    ui = BuilderUI()
+    ui.show()
+    sys.exit(app.exec())
--- train_system/track_model/track_model_ui.py
+++ train_system/track_model/track_model_ui.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from PyQt6.QtCore import pyqtSignal, Qt
+from PyQt6 import QtCore
 from PyQt6.QtWidgets import (QMainWindow, QWidget, QStackedWidget, QApplication)
 from train_system.common.line import Line
 from train_system.track_model.murphy_view import MurphyUI
@@ -8,7 +8,7 @@ from train_system.track_model.builder_view import BuilderUI
 
 class TrackModelUI(QMainWindow):
 
-    def __init__(self, line: Line):
+    def __init__(self):
 
         super().__init__()
 
@@ -16,21 +16,22 @@ class TrackModelUI(QMainWindow):
         self.setGeometry(100, 100, 800, 600)
 
         self.views = QStackedWidget()
-        self.murphy = MurphyUI(line)
-        self.builder = BuilderUI(line)
+        self.murphy = MurphyUI()
+        self.builder = BuilderUI()
         self.views.addWidget(self.murphy)
         self.views.addWidget(self.builder)
 
-        self.views.setCurrentWidget(self.murphy)
+        self.views.setCurrentWidget(self.builder)
+        self.setCentralWidget(self.views)
 
 
 if __name__ == "__main__":
 
-    line = Line('Green')
-    file_path = os.path.abspath(os.path.join("system_data/lines", "green_line.xlsx"))
-    line.load_track_blocks(file_path)
+    #line = Line('Green')
+    #file_path = os.path.abspath(os.path.join("system_data/lines", "green_line.xlsx"))
+    #line.load_track_blocks(file_path)
     
     app = QApplication(sys.argv)
-    ui = TrackModelUI(line)
+    ui = TrackModelUI()
     ui.show()
     sys.exit(app.exec())
