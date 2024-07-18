a956b8a5ba6cb56a2a0cd9f963b7ea252d610336
Arissa <ajb321@pitt.edu>
2024-07-18 01:13:51 -0400

    authority updated

--- train_system/mbo_manager/mbo_main.py
+++ train_system/mbo_manager/mbo_main.py
@@ -6,7 +6,6 @@ from PyQt6.QtCore import Qt, QObject, pyqtSlot, pyqtSignal
 from train_system.common.time_keeper import TimeKeeper
 from train_system.common.line import Line
 from train_system.common.track_block import TrackBlock
-
 from train_system.mbo_manager.mbo_manager import MBOOffice
 from train_system.mbo_manager.mbo_ui import MBOWindow
 
@@ -25,10 +24,12 @@ time_keeper = TimeKeeper()
 time_keeper.start_timer()
 
 #instantiate MBOController
-mbo_office = MBOOffice()
+mbo_office = MBOOffice(time_keeper)
 schedules = MBOOffice.Schedules()
 satellite = MBOOffice.Satellite()
 satellite.mbo_mode = True
+
+time_keeper.tick.connect(mbo_office.handle_time_update)
 
 #train example for testing? 
 train = Train()
